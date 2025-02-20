b51efdf1ac97882dcb142fd90f6aa3e0b9af680a
poojapatil <poojapatilpp6640@gmail.com>
2025-02-20 11:14:02 +0530

    included deepseek r1 model to get the responses depending on user query

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request  
+import json  
+from ollama import chat
+from ollama import ChatResponse
 
+ 
 app = FastAPI()
 
 @app.get("/")  # Ensure the decorator is correct
 async def root():
     return "Application is up and running!"
 
+## using deepseek-r1:1.5b model to get information about Data Science depending on user query.
+@app.post("/Information")  # Ensure the decorator is correct
+async def info(request: Request):  
+    try:
+        request_json = await request.json()  
+        user_query = request_json.get('query', None)  
+        
+        if not user_query:
+            return {"error": "Query parameter is missing"}  
 
-@app.get("/Information")
-async def info():
+        result = await ds_info(user_query) 
+        return {"message": "Success","query": user_query, "data": result}
+
+    except Exception as e:
+        return {"error": str(e)}
     
+async def ds_info(user_query):
+    print(user_query)
+    response = chat(model='deepseek-r1:1.5b', messages=[
+    {
+        'role': 'user',
+        'content': user_query,
+    },
+    ])
+    print(response.message.content)
 
-    return "Information needs to added here"
+    return response.message.content
+        
--- outputjson.py
+++ outputjson.py
@@ -3,6 +3,6 @@ import requests
 request = requests.get('http://127.0.0.1:8000/')
 print(request.json())        # Output: Application is up and running!
 
-info = requests.get('http://127.0.0.1:8000/Information')
+info = requests.post('http://127.0.0.1:8000/Information')
 print(info.json())         # Output: Information needs to added here
                     
